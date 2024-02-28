866fb04caa14d8d229b098c6c22975236c3770a8
raf <daiyanrafi1@gmail.com>
2024-02-28 12:31:32 +0600

    final wednesday 12.31 / 28-02-24

--- src/App.tsx
+++ src/App.tsx
@@ -20,16 +20,21 @@
 import React from 'react';
 import ScheduleManagement from './schedule/ScheduleManagement';
 import scheduleData from './data/Alan.json'; // Adjust the path as needed
+import ScheduleDashboard from './Dashboard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         {/* <p>Scheduling System is coming here!!!</p> */}
+
         <ScheduleManagement schedule={scheduleData} />
+        <ScheduleDashboard />
       </header>
     </div>
   );
 }
 
 export default App;
+

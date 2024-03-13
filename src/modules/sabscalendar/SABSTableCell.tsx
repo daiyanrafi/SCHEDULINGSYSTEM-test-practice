aec754d94d539e803587963d708b9a74f1629e14
raf <daiyanrafi1@gmail.com>
2024-03-13 11:41:10 +0600

    onlcik added

--- src/modules/sabscalendar/SABSTableCell.tsx
+++ src/modules/sabscalendar/SABSTableCell.tsx
@@ -4,49 +4,65 @@ import ScheduleManagement, { ScheduleItem } from "../../schedule/ScheduleManagem
 import { IScheduleColumn } from "./SABSCalendar";
 
 const SABSTableCell: React.FC<IProp> = (prop) => {
-  const schedule = prop.schedule;
-  const date = new Date(schedule.starttime);
-  return <TableCell
-    key={`${date.getDay()}-${date.getHours()}-${date.getMinutes()}`}
-    colSpan={schedule.callspan}
-    sx={{
-      fontFamily: "Calibri",
-      border: "1px solid #E0E0E0",
-      padding: "0px",
-      backgroundColor: "#FAFAFA",
-      verticalAlign: "top",
-    }}
-  >
-    <div style={{
-              backgroundColor: "#B2D8F4",
-              borderLeft: "5px solid #2582D7",
-              margin: "8px 2px",
-              paddingLeft: "02%",
-              fontFamily: "Calibri",
-              transition: "background-color 0.3s",
-              borderRadius: "8px",
-              padding: "02px 05px",
-            }}>{schedule.name} {schedule.callspan}</div>
-  </TableCell>
-}
+  const { schedule } = prop;
+
+  const handleClick = () => {
+    console.log("Appoinment clicked");
+  };
+
+  return (
+    <TableCell
+      onClick={handleClick}
+      key={`${schedule.starttime}-${schedule.name}`}
+      colSpan={schedule.callspan}
+      sx={{
+        fontFamily: "Calibri",
+        border: "1px solid #E0E0E0",
+        padding: "0px",
+        backgroundColor: "#FAFAFA",
+        verticalAlign: "top",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#B2D8F4",
+          borderLeft: "5px solid #2582D7",
+          margin: "8px 2px",
+          paddingLeft: "02%",
+          fontFamily: "Calibri",
+          transition: "background-color 0.3s",
+          borderRadius: "8px",
+          padding: "02px 05px",
+        }}
+      >
+        {schedule.name} {schedule.callspan}
+      </div>
+    </TableCell>
+  );
+};
 
 export const SABSEmptyTableCell: React.FC<IEmptyCell> = (prop) => {
-  const header = prop.header;
-  //const date = new Date(schedule.starttime);
-  return <TableCell
-    key={`${header.day}-${header.hour}-${header.minute}`}
-    colSpan={1}
-    sx={{
-      fontFamily: "Calibri",
-      border: "1px solid #E0E0E0",
-      padding: "0px",
-      backgroundColor: "#FAFAFA",
-      verticalAlign: "top",
-    }}
-  >
+  const { header } = prop;
 
-  </TableCell>
-}
+  const handleClick = () => {
+    console.log("empty cell clicked");
+  };
+
+  return (
+    <TableCell
+      onClick={handleClick}
+      key={`${header.day}-${header.hour}-${header.minute}`}
+      colSpan={1}
+      sx={{
+        fontFamily: "Calibri",
+        border: "1px solid #E0E0E0",
+        padding: "0px",
+        backgroundColor: "#FAFAFA",
+        verticalAlign: "top",
+      }}
+    ></TableCell>
+  );
+};
 
 export default SABSTableCell;
 

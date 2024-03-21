05ca122098d497c1988b96d6ceaa9438c1f17d66
raf <daiyanrafi1@gmail.com>
2024-03-21 11:36:02 +0600

    allignment changes

--- src/Charges/ChargesTable.tsx
+++ src/Charges/ChargesTable.tsx
@@ -8,6 +8,14 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   height: '40px', // Adjust the height here as per your requirement
 }));
 
+// Custom styled checkbox
+const CustomCheckbox = styled(Checkbox)(({ theme }) => ({
+  padding: 0,
+  '& .MuiSvgIcon-root': {
+    fontSize: '16px', // Adjust the size here
+  },
+}));
+
 interface Charge {
   id: string;
   amount: number;
@@ -24,24 +32,24 @@ interface Props {
 
 const ChargesTable: React.FC<Props> = ({ charges }) => {
   return (
-    <TableContainer component={Paper} style={{ marginTop: '20px', overflowX: 'auto' }}>
-      <Table style={{ fontFamily: 'Calibri' }}>
+    <TableContainer component={Paper} style={{ marginTop: '40px', overflowX: 'auto', margin: '20px auto', width: '95%', maxWidth: '1500px' }}>
+      <Table style={{ fontFamily: 'Calibri', border: '1px solid #ccc' }}>
         <TableHead>
           <TableRow>
-            <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>ID</TableCell>
-            <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>Created</TableCell>
-            <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>Amount</TableCell>
-            <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>Currency</TableCell>
-            <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>Customer</TableCell>
-            <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>Paid</TableCell>
-            <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>Document No.</TableCell>
-            <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>Journal Document No.</TableCell>
+            <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '10%' }}>ID</TableCell>
+            <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '15%' }}>Created</TableCell>
+            <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '5%' }}>Amount</TableCell>
+            <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '5%' }}>Currency</TableCell>
+            <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '10%' }}>Customer</TableCell>
+            <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '5%' }}>Paid</TableCell>
+            <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '10%' }}>Document No.</TableCell>
+            <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '5%' }}>Journal Document No.</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {charges.map((charge) => (
             <StyledTableRow key={charge.id}>
-              <TableCell style={{ fontFamily: 'Calibri', padding: '10px', color: 'blue', textDecoration: 'underline', cursor: 'pointer' }}>
+              <TableCell style={{ fontFamily: 'Calibri', padding: '10px', color: 'blue', textDecoration: 'underline', cursor: 'pointer', width: '10%' }}>
                 {charge.id}
               </TableCell>
               <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>{new Date(charge.created * 1000).toLocaleString()}</TableCell>
@@ -49,10 +57,10 @@ const ChargesTable: React.FC<Props> = ({ charges }) => {
               <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>{charge.currency}</TableCell>
               <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>{charge.customer}</TableCell>
               <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>
-                <Checkbox checked={charge.paid} disabled />
+                <CustomCheckbox checked={charge.paid} disabled /> {/* Use custom styled checkbox */}
               </TableCell>
-              <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>{charge.invoice}</TableCell>
-              <TableCell style={{ padding: '10px', fontFamily: 'Calibri' }}>{charge.invoice}</TableCell>
+              <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '10%' }}>{charge.invoice}</TableCell>
+              <TableCell style={{ padding: '10px', fontFamily: 'Calibri', width: '5%' }}>{charge.invoice}</TableCell>
             </StyledTableRow>
           ))}
         </TableBody>
